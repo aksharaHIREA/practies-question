48ae610fb0cb62b32e0bb51a0e063f8926c2f15f
Akshara Jangid <88184872+aksharaHIREA@users.noreply.github.com>
2022-11-18 22:49:34 +0530

    Add files via upload

---
+++ practies question index.js/index.js
@@ -0,0 +1,169 @@
+//Create a function named as ”isEven” which a accept a parameter named ”num”, function
+//check whether num is even or not, If “num” is even function return true , else function
+//return false
+
+// function isEven (num){
+//     if (num %2==0){
+//         return true
+//     }
+//     else{
+//         return false
+//     }
+// }
+
+// console.log(isEven(24))
+
+
+
+// Create a function named as ”isOdd” which a accept a parameter named ”num”, function
+// check whether num is odd or not, If “num” is odd function return true , else function
+// return false.
+
+// function isOdd (num){
+//     if(num %2==1){
+//         return true
+//     }
+//     else{
+//         return false
+//     }
+// }
+
+// console.log(isOdd (34))
+
+
+
+// Write a program which print all the number between 2 to 200 which are divisible by 2 and
+// divisible by 3 but not divisible by 5.
+
+// for (var i=2; i<200; i++){
+//     if( i%2==0 && i%3==0 && i%5!=0 )
+//     {
+//        console.log(i)  
+//     }
+// }
+
+
+
+// Write a program which print all the number between 200 to 500 which are divisible by 2,
+// divisible by 3,  divisible by 5 but not divisible by 8
+
+// for (var i=200; i<500; i++){
+//     if(i%2==0 && i%3==0 && i%5==0 && i%8!=0)
+//     {
+//         console.log(i)
+//     }
+// }
+
+
+
+// Write a functions for 1 to 10 divisiblility
+
+// function divisiblility (num){
+//     if(num %1==0){
+//         return true
+//     }
+//     else{
+//         return false
+//     }
+// }
+
+// console.log(divisiblility (28))
+
+// function divisiblility(num){
+//     if(num %2==0){
+//         return true
+//     }
+//     else{
+//         return false
+//     }
+// }
+
+// console.log(divisiblility(36))
+
+// function divisiblility(num){
+//     if(num %3==0){
+//         return true
+//     }
+//     else{
+//         return false
+//     }
+// }
+
+// console.log(divisiblility(13))
+
+// function divisiblility(num){
+//     if(num %4==0){
+//         return true
+//     }
+//     else{
+//         return false
+//     }
+// }
+
+// console.log(divisiblility(17))
+
+// function divisiblility(num){
+//     if(num %5==0){
+//         return true
+//     }
+//     else{
+//         return false
+//     }
+// }
+
+// console.log(divisiblility(63))
+
+// function divisiblility(num){
+//     if(num %6==0){
+//         return true
+//     }
+//     else{
+//         return false
+//     }
+// }
+
+// console.log(divisiblility(70))
+
+// function divisiblility(num){
+//     if(num %7==0){
+//         return true
+//     }
+//     else{
+//         return false
+//     }
+// }
+
+// console.log(divisiblility(32))
+
+// function divisiblility(num){
+//     if(num %8==0){
+//         return true
+//     }
+//     else{
+//         return false
+//     }
+// }
+
+// console.log(divisiblility(19))
+
+// function divisiblility(num){
+//     if(num %9==0){
+//         return true
+//     }
+//     else{
+//         return false
+//     }
+// }
+
+// console.log(divisiblility(29))
+
+// function divisiblility(num){
+//     if(num %10==0){
+//         return true
+//     }
+//     else{
+//         return false
+//     }
+// }
+
+// console.log(divisiblility(81))
